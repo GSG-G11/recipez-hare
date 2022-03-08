b15765deba5c479f242fde978762b255a0dca2ba
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-03-08 20:25:18 +0200

    sign up route handle Relates #6

---
+++ src/controllers/sign-up.js
@@ -0,0 +1,15 @@
+const { addNewUserQuery, hasUsernameTakenQuery } = require('../database/queries');
+const { signUpValidation } = require('../validation');
+const { hashPassword } = require('../utils');
+
+const signUp = (req, res) => {
+  const { username, password } = req.body;
+  signUpValidation(req.body)
+    .then(() => hasUsernameTakenQuery({ username }))
+    .then(() => hashPassword(password))
+    .then((hashedPassword) => addNewUserQuery({ username, hashedPassword }))
+    .then((dbRes) => res.json(dbRes))
+    .catch((dbError) => res.json(`${dbError}`));
+};
+
+module.exports = { signUp };
---
+++ src/database/queries/index.js
@@ -0,0 +1,4 @@
+const { hasUsernameTakenQuery } = require('./hasUsernameTakenQuery');
+const { addNewUserQuery } = require('./addNewUserQuery');
+
+module.exports = { hasUsernameTakenQuery, addNewUserQuery };
