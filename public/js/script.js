853dc952c8871a678acf6ba700bfbd6e5a5a0777
farah2003 <farah.shaqoura@example.com>
2022-03-09 17:43:06 +0200

    add login-style

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,11 @@
 import postData from './postData.js';
 import getUserInfoFromCookie from './getUserInfoFromCookie.js';
 
+const plusIcon = document.querySelector('#plus');
+const recipySection = document.querySelector('.createRecipy');
+const closeIcon = document.querySelector('#close');
+const addBtn = document.querySelector('#btn');
+
 console.log(getUserInfoFromCookie());
 
 const login = document.querySelector('#login');
@@ -22,4 +27,21 @@ reciepes?.addEventListener('submit', (e) => {
   e.preventDefault();
   const { title, detail, userId } = e.target;
   postData({ title: title.value.trim(), detail: detail.value.trim(), userId: Number(userId.value.trim()) }, '/user/addReciepes');
+});
+
+plusIcon.addEventListener('click', () => {
+  recipySection.style.display = 'flex';
+
+  plusIcon.style.color = '#000';
+});
+
+addBtn.addEventListener('click', () => {
+  recipySection.style.display = 'none';
+
+  plusIcon.style.color = '#FFC300';
+});
+closeIcon.addEventListener('click', () => {
+  recipySection.style.display = 'none';
+
+  plusIcon.style.color = '#FFC300';
 });
