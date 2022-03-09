b926cc4b3f7bbd4f5a3277ba0015be06a7805b18
WessalJawad95 <wessaljawad95@gmail.com>
2022-03-09 12:02:23 +0200

    check auth middleware relates #18

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { signUp, logIn, loginHandle } = require('../controllers');
+const { userRouter } = require('./user');
 
 router.get('/log-in', loginHandle);
 router.post('/sign-up', signUp);
 router.post('/log-in', logIn);
+router.use('/user', userRouter);
 
 module.exports = router;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,8 @@
 const { hashPassword } = require('./hashPassword');
 const { comparepassword } = require('./comparepassword');
 const { jwtSignPromise } = require('./jwtSignPromise');
+const { jwtVerifyPromise } = require('./jwtVerifyPromise');
 
-module.exports = { hashPassword, jwtSignPromise, comparepassword };
+module.exports = {
+  hashPassword, jwtSignPromise, comparepassword, jwtVerifyPromise,
+};
